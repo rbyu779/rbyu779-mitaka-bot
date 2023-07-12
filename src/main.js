2ff45a4b399e93b9f6f57476d4f7a23a327b0b43
b1354 <bayurizkyseptiansyah@gmail.com>
2023-07-12 19:30:30 +0700

    coba image

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const client = require("./lib/client.js");
+const express = require("express");
+
+const app = express();
 
 console.log("Preparing bot,");
 console.log("Please wait...");
 
 client.initialize();
+
+app.get('/', (req, res) => {
+  res.send("bot running!");
+})
+
+app.listen(8000, () => console.log("app listening on port: 8000"));
